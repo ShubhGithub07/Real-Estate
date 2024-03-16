36ccbcaf7ec90a66b54077ed73853b4dd691f6b7
ShubhGithub07 <127323514+ShubhGithub07@users.noreply.github.com>
2024-03-16 12:18:26 +0530

    components complete

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import "./list.scss";
+import Card from "../card/Card";
+import { listData } from "../../lib/dummydata";
 
 function List() {
   return (
-    <div>List</div>
-  )
+    <div className="list">
+      {listData.map((item) => (
+        <Card key={item.id} item={item} />
+      ))}
+    </div>
+  );
 }
 
-export default List
+export default List;
